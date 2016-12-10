9dc881bb0da433c76aeed7efa540f79b8906de0a
tibo <pillon.tibo@gmail.com>
2016-12-10 01:12:29 +0100

    custom tree started

---
+++ CameraArchery/UsersControl/CustomBrowser.xaml.cs
@@ -0,0 +1,107 @@
+﻿using CameraArchery.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CameraArchery.UsersControl
+{
+    /// <summary>
+    /// Interaction logic for Browser.xaml
+    /// </summary>
+    public partial class CustomBrowser : System.Windows.Controls.UserControl, INotifyPropertyChanged
+    {
+        private Brush foreground;
+        public Brush Foreground
+        {
+            get
+            {
+                return foreground;
+            }
+            set
+            {
+                foreground = value;
+                OnPropertyChanged("Foreground");
+            }
+        }
+
+        /// <summary>
+        /// event to dataBinding
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string defaultFile;
+        public string DefaultFile
+        {
+            get
+            {
+                return defaultFile;
+            }
+            set
+            {
+                defaultFile = value;
+                fileName = DefaultFile;
+            }
+        }
+
+
+        private string fileName;
+        public string FileName 
+        { 
+            get
+            {
+                return fileName;
+            }
+            set
+            {
+                fileName = value;
+                OnPropertyChanged("FileName");
+            }
+        }
+
+
+        public CustomBrowser()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            Foreground = Brushes.Black;
+        }
+
+        private void ShowDialog()
+        {
+            new CustomBrowserFolder("video").ShowDialog();
+        }
+        
+        /// <summary>
+        /// function to dataBinding
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ShowDialog();
+        }
+
+    }
+}
---
+++ CameraArchery/View/CustomBrowserFolder.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CameraArchery.View
+{
+    /// <summary>
+    /// Interaction logic for CustomBrowserFolder.xaml
+    /// </summary>
+    public partial class CustomBrowserFolder : Window
+    {
+        public CustomBrowserFolder(string uri)
+        {
+            InitializeComponent();
+
+
+
+            TreeControl.Items.Add(GetFolder(uri));
+
+        }
+
+        private MenuItem GetFolder(string uri)
+        {
+            MenuItem root = new MenuItem(){ Title = uri.Split('\\').Last() };
+
+            foreach(var dir in Directory.EnumerateDirectories(uri))
+                root.Items.Add(GetFolder(dir));
+
+            return root;
+        }
+    }
+
+
+    public class MenuItem
+    {
+        public string Title { get; set; }
+        public ObservableCollection<MenuItem> Items { get; set; }
+        public MenuItem()
+        {
+            this.Items = new ObservableCollection<MenuItem>();
+        }
+
+    }
+}
