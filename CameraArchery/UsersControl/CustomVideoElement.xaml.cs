116170d607152933a328ecdaf8ecc2006ed06cdf
tibo <pillon.tibo@gmail.com>
2017-03-09 12:59:37 +0100

    change the loaded of bitmap image

--- CameraArchery/UsersControl/CustomVideoElement.xaml.cs
+++ CameraArchery/UsersControl/CustomVideoElement.xaml.cs
@@ -2,24 +2,10 @@
 using CameraArchery.Adorners;
 using CameraArchery.Behaviors;
 using CameraArcheryLib;
-using CameraArcheryLib.Controller;
-using CameraArcheryLib.Factories;
 using CameraArcheryLib.Utils;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Interactivity;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CameraArchery.UsersControl
